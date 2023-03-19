e7c859ca9c9f05bbe6c14696f362eb2d3a8b9c56
LongXiangL <bfh1104@gmail.com>
2023-03-19 21:29:13 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')//載入express
+const session = require('express-session')
 const exphbs = require('express-handlebars')// 引用 handlebars
 const bodyParser = require('body-parser')// 引用 body-parser
 const methodOverride = require('method-override')//載入method-override
-
+const PORT = process.env.PORT || 3000
 
 
 
@@ -16,15 +17,17 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))//每一筆請求先用body-parser進行前置處理
 app.use(methodOverride('_method'))//設定每一筆請求都會透過methodOverride進行前置處理
 app.use(routes)// 將 request 導入路由器
+app.use(session({
+  secret:"ThisIsMySecret",
+  resave:false,
+  saVeUninitialized:true
+}))
 
 
 
 
 
-// app.js
-// 如果在 Heroku 環境則使用 process.env.PORT
-// 否則為本地環境，使用 3000 
-const PORT = process.env.PORT || 3000
+
 
 
 app.listen(PORT, () => {
