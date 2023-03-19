a7a0c495cb058b110edd1472536e00ce2dbbf063
LongXiangL <bfh1104@gmail.com>
2023-03-20 00:15:24 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')//載入express
 const session = require('express-session')
+const usePassport=require('./config/passport')
 const exphbs = require('express-handlebars')// 引用 handlebars
 const bodyParser = require('body-parser')// 引用 body-parser
 const methodOverride = require('method-override')//載入method-override
@@ -16,6 +17,8 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))//樣板引
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))//每一筆請求先用body-parser進行前置處理
 app.use(methodOverride('_method'))//設定每一筆請求都會透過methodOverride進行前置處理
+
+usePassport(app)//呼叫Passport函式並傳入app，需放在路由之前處理
 app.use(routes)// 將 request 導入路由器
 app.use(session({
   secret:"ThisIsMySecret",
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,13 +1,19 @@
 const express = require('express')
+const passport=require('passport')
 const router = express.Router()
 const User = require('../../models/user')
+
+
+
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
-
-router.post('/login', (req, res) => {
-
-})
+// 加入 middleware，驗證 request 登入狀態
+router.post('/login',passport.authenticate('local',{
+successRedirect:'/',
+failureRedirect:'/users/login'
+ }))
 
 
 router.get('/register', (req, res) => {
@@ -40,5 +46,6 @@ router.post('/register', (req, res) => {
     .catch(err => console.log(err))
 
 })
+
 
 module.exports = router
