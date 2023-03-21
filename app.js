a51e0bfb254c027b4685684a0640e84a84d51d71
LongXiangL <bfh1104@gmail.com>
2023-03-21 10:54:41 +0800

    feat: 修改bug session位置導致無法登入問題

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')//載入express
 const session = require('express-session')
-const usePassport=require('./config/passport')
+const usePassport = require('./config/passport')
 const exphbs = require('express-handlebars')// 引用 handlebars
 const bodyParser = require('body-parser')// 引用 body-parser
 const methodOverride = require('method-override')//載入method-override
@@ -18,27 +18,22 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))//每一筆請求先用body-parser進行前置處理
 app.use(methodOverride('_method'))//設定每一筆請求都會透過methodOverride進行前置處理
 
+app.use(session({
+  secret: "ThisIsMySecret",
+  resave: false,
+  saveUninitialized: true
+}))
+
 usePassport(app)//呼叫Passport函式並傳入app，需放在路由之前處理
-app.use((req,res,next)=>{
+
+app.use((req, res, next) => {
   console.log(req.user)
-  res.locals.isAuthenticated=req.isAuthenticated()
-  res.locals.user=req.user
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
   next()
 })
 
 app.use(routes)// 將 request 導入路由器
-app.use(session({
-  secret:"ThisIsMySecret",
-  resave:false,
-  saVeUninitialized:true
-}))
-
-
-
-
-
-
-
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ const router = express.Router()// 引用Express 路由器
 const home = require('./modules/home')// 引入 home 模組程式碼
 const todos = require('./modules/todos')
 const users = require('./modules/users')
-const {authenticator}=require('../middleware/auth')
+const { authenticator } = require('../middleware/auth')
 
 router.use('/todos', authenticator, todos)
-router.use('/users',users)
+router.use('/users', users)
 router.use('/', authenticator, home)
 
-module.exports = router// 匯出路由器
+module.exports = router
